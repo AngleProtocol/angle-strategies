1faaacde0fb70aac9eaef13234495281a6c1b722
Guillaume Nervo <guillaume.nervo@gmail.com>
2022-05-10 14:15:18 +0200

    lint errors

--- scripts/mainnet-fork/harvest.ts
+++ scripts/mainnet-fork/harvest.ts
@@ -1,50 +1,35 @@
 // This script is to be run after having run `unpauseCollat.ts`
 import {
-  PerpetualManagerFront,
-  PerpetualManagerFront__factory,
   PoolManager,
   // eslint-disable-next-line camelcase
   PoolManager_Interface,
-  StableMasterFront,
-  StableMasterFront__factory,
 } from '@angleprotocol/sdk/dist/constants/interfaces';
 
-import { expect } from '../../test/test-utils/chai-setup';
 import { CONTRACTS_ADDRESSES, ChainId } from '@angleprotocol/sdk';
-import { network, ethers, deployments } from 'hardhat';
+import { network, ethers } from 'hardhat';
 import { parseUnits } from 'ethers/lib/utils';
 import { ERC20, ERC20__factory, OptimizerAPRStrategy, OptimizerAPRStrategy__factory } from '../../typechain';
-import { time } from '../../test/test-utils/helpers';
-import { DAY } from '../../test/contants';
 import { logBN } from '../../test/utils-interaction';
 
 async function main() {
   // =============== Simulation parameters ====================
-  const { deployer, keeper: fakeKeeper } = await ethers.getNamedSigners();
+  const { deployer } = await ethers.getNamedSigners();
 
-  const stableName = 'EUR';
   const collateralName = 'USDC';
 
   let strategyAddress: string;
   let poolManagerAddress: string;
-  let keeper: string;
 
-  // if fork we suppose that we are in mainnet
-  let json = (await import('../../deploy/networks/mainnet.json')) as any;
   if (!network.live) {
     poolManagerAddress = CONTRACTS_ADDRESSES[ChainId.MAINNET].agEUR?.collaterals?.[collateralName]
       ?.PoolManager as string;
     strategyAddress = CONTRACTS_ADDRESSES[ChainId.MAINNET].agEUR?.collaterals?.[collateralName]?.Strategies
       ?.GenericOptimisedLender as string;
-    keeper = '0xcC617C6f9725eACC993ac626C7efC6B96476916E';
   } else {
     poolManagerAddress = CONTRACTS_ADDRESSES[network.config.chainId as ChainId].agEUR?.collaterals?.[collateralName]
       ?.PoolManager as string;
     strategyAddress = CONTRACTS_ADDRESSES[network.config.chainId as ChainId].agEUR?.collaterals?.[collateralName]
       ?.Strategies?.GenericOptimisedLender as string;
-
-    json = await import('./networks/' + network.name + '.json');
-    keeper = fakeKeeper.address;
   }
 
   const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
