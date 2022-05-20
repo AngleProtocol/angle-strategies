4473f27488fb650c6e421015d119085158550f51
Guillaume Nervo <guillaume.nervo@gmail.com>
2022-05-20 10:58:54 +0200

    deployed frax staker

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -59,7 +59,7 @@ const config: HardhatUserConfig = {
       forking: {
         enabled: argv.fork || false,
         url: nodeUrl('fork'),
-        blockNumber: 14563630,
+        blockNumber: 14810016,
       },
       mining: argv.disableAutoMining
         ? {
