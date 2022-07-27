d2fe848df89d1502526184e698bfd7cd9039404b
Pablo Veyrat <veyratpablo@gmail.com>
2022-07-27 19:20:36 +0200

    fixing lint

--- contracts/interfaces/IVotingEscrow.sol
+++ contracts/interfaces/IVotingEscrow.sol
@@ -8,6 +8,7 @@ pragma solidity 0.8.12;
 interface IVotingEscrow {
     //solhint-disable-next-line
     function user_point_epoch(address user) external view returns (uint256);
+
     //solhint-disable-next-line
     function user_point_history__ts(address user, uint256 epoch) external view returns (uint256);
 }
--- contracts/mock/MockStrategy4626.sol
+++ contracts/mock/MockStrategy4626.sol
@@ -6,11 +6,11 @@ import "../savingsRate/BaseStrategy4626.sol";
 /// @title Angle Strategy ERC4626
 /// @author Angle Protocol
 contract Strategy4626 is BaseStrategy4626 {
-    function estimatedAPR() external view override returns (uint256) {
-        return 0;
-    }
+    uint256 public count;
 
-    function estimatedTotalAssets() public view override returns (uint256) {}
+    function estimatedAPR() external view override returns (uint256) {
+        return count;
+    }
 
     /// @notice Returns the amount of underlying tokens that idly sit in the Vault.
     /// @return The amount of underlying tokens that sit idly in the Vault.
@@ -22,19 +22,18 @@ contract Strategy4626 is BaseStrategy4626 {
 
     function _adjustPosition() internal override {}
 
-    function _liquidatePosition(uint256 _amountNeeded)
-        internal
-        override
-        returns (uint256 _liquidatedAmount, uint256 _loss)
-    {
+    function _liquidatePosition(uint256) internal override returns (uint256 _liquidatedAmount, uint256 _loss) {
+        count += 1;
         return (0, 0);
     }
 
     function _liquidateAllPositions() internal override returns (uint256 _amountFreed) {
+        count += 1;
         return 0;
     }
 
     function _protectedTokens() internal view override returns (address[] memory) {
+        count;
         address[] memory protected = new address[](0);
         return protected;
     }
