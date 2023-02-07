6e98e1fe2aa9c29473603aa20e416e9ef42e5372
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2023-02-07 17:10:41 +0100

    comment @greedythib

--- contracts/strategies/OptimizerAPR/genericLender/euler/GenericEulerStaker.sol
+++ contracts/strategies/OptimizerAPR/genericLender/euler/GenericEulerStaker.sol
@@ -84,8 +84,8 @@ contract GenericEulerStaker is GenericEuler {
         // APRs are in 1e18 and a 5% penalty on the EUL price is taken to avoid overestimations
         // `_estimatedEulToWant()` and eTokens are in base 18
         apr =
-            (_estimatedEulToWant(eulerStakingContract.rewardRate() * _SECONDS_IN_YEAR) * 9500 * 10**6) /
-            10000 /
+            (_estimatedEulToWant(eulerStakingContract.rewardRate() * _SECONDS_IN_YEAR) * 9_500 * 10**6) /
+            10_000 /
             newTotalSupply;
     }
 
